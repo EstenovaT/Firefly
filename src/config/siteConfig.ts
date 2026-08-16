22c6544d7c839964639582cb9fae357d122a5aa5
EstenovaT <3468239417@qq.com>
2026-08-17 02:06:32 +0800

    test

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -40,21 +40,21 @@ const pages = resolvePageToggles({
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "Estenova",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "EstenovaBlog",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://estenova.top",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"Estenova个人博客网站。",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
+		"Estenova",
 		"Fuwari",
 		"Astro",
 		"ACGN",
